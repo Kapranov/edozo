52a6bfaaa427908d88cd0c1313c79c0fc96757ae
Oleg G.Kapranov <oleg@vertiopia.com>
2017-03-21 10:15:53 +0200

    added slim templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+
   %w( 404 422 500 ).each do |code|
     get code, to: "errors#show", :code => code
   end
-
-  get 'home/index'
 
   root to: 'home#index'
 end
